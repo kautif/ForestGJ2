3ac9d8041ec85764044bbc16e8e5b85e2898d998
Alexander Gregg <agregg444@gmail.com>
2023-06-27 21:23:49 -0500

    Resolved some of the unity weirdness from merge. Added Overworld level completeion material.

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -10,26 +10,44 @@ public class LevelSelect : MonoBehaviour
     [SerializeField] private GameObject levelConfirm;
     [SerializeField] private GameObject player;
     [SerializeField] private TextMeshProUGUI levelText;
-    [SerializeField] private int levelNum;
+    public int levelNum;
+    public Material levelComplete;
 
     public Image levelStartImg;
 
     void Start()
     {
+        //---LEVEL PROGRESSION CLEARING CODE!---
+        //PlayerPrefs.SetInt("Level 0", 0); // Overworld
+        //PlayerPrefs.SetInt("Level 1", 0); // Level 1
+        //PlayerPrefs.SetInt("Level 2", 0); // Level 2
+        //PlayerPrefs.SetInt("Level 3", 0); // Level 3
+        //PlayerPrefs.SetInt("Level 4", 0); // Level 4
+        //PlayerPrefs.SetInt("Level 5", 0); // Level 5
+        //--------------------------------------
         
+        //Sets level completetion material to level node when the level is complete. //This is using the build index.
+        string currentLevelIndex = "Level " + this.gameObject.GetComponent<LevelSelect>().levelNum;
+        if(PlayerPrefs.GetInt(currentLevelIndex) > 0){
+            //The player has beat this level
+            this.gameObject.GetComponent<MeshRenderer>().material = levelComplete;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100))
             {
+                
                 if (hit.collider.CompareTag("Map Node"))
                 {
+                    
                     switch (hit.collider.gameObject.name) {
                         case "1":
                             levelNum = 1;
@@ -42,6 +60,9 @@ public class LevelSelect : MonoBehaviour
                             break;
                         case "4":
                             levelNum = 4;
+                            break;
+                        case "5":
+                            levelNum = 5;
                             break;
                         default: 
                             levelNum = 0; 
